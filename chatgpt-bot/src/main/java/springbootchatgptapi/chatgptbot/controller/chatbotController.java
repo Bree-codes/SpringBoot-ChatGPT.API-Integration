b60f4801d64647e01282e45f34d77f2407f0c5b4
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-19 10:20:17 +0300

    to be continued

--- chatgpt-bot/src/main/java/springbootchatgptapi/chatgptbot/controller/chatbotController.java
+++ chatgpt-bot/src/main/java/springbootchatgptapi/chatgptbot/controller/chatbotController.java
@@ -1,12 +1,32 @@
 package springbootchatgptapi.chatgptbot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import springbootchatgptapi.chatgptbot.DTO.ChatgptRequest;
 import springbootchatgptapi.chatgptbot.DTO.ChatgptResponse;
 
 @RestController
 @RequestMapping("/chatbot")
 public class chatbotController {
 
-    public ChatgptResponse chatgptResponse;
+    @Value("${openai.model}")
+    private String model;
+
+    @Value(("${openai.api.url}"))
+    private String apiUrl;
+
+    @Autowired
+    private RestTemplate template;
+
+    public ChatgptResponse chatgptResponse(@RequestParam("prompt") String prompt){
+
+        ChatgptRequest chatgptRequest = new ChatgptRequest(model,prompt);
+        ChatgptResponse chatgptResponse=  template.postForObject(apiUrl,chatgptRequest, ChatgptResponse.class);
+        return null;
+
+    }
 }
